99fcd51a0d7e2219fa81ff9fe08a6b99df0c1e42
Oklevet <nikstar1235@gmail.com>
2023-10-16 10:23:37 +0300

    2. HQL [#6874 #452202]

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -3,10 +3,6 @@ package ru.job4j.cars.repository;
 import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.query.Query;
 import ru.job4j.cars.model.User;
 
 import java.util.List;
@@ -14,13 +10,7 @@ import java.util.Optional;
 
 @AllArgsConstructor
 public class UserRepository {
-    private Session session;
-
     private final SessionFactory sf;
-
-    public UserRepository(SessionFactory sf) {
-        this.sf = sf;
-    }
 
     /**
      * Сохранить в базе.
@@ -28,15 +18,16 @@ public class UserRepository {
      * @return пользователь с id.
      */
     public User create(User user) {
+        Session session = sf.openSession();
         try {
-            session = sf.openSession();
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
-            session.close();
             return user;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
         return user;
     }
@@ -46,14 +37,15 @@ public class UserRepository {
      * @param user пользователь.
      */
     public void update(User user) {
+        Session session = sf.openSession();
         try {
-            session = sf.openSession();
             session.beginTransaction();
             session.update(user);
             session.getTransaction().commit();
-            session.close();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
     }
 
@@ -62,16 +54,17 @@ public class UserRepository {
      * @param userId ID
      */
     public void delete(int userId) {
+        Session session = sf.openSession();
         try {
-            session = sf.openSession();
             session.beginTransaction();
-            User user = new User();
-            user.setId(userId);
-            session.delete(user);
+            session.createQuery("delete user as u where u.id = :id")
+                    .setParameter("id", userId)
+                    .executeUpdate();
             session.getTransaction().commit();
-            session.close();
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
     }
 
@@ -80,18 +73,18 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findAllOrderById() {
+        Session session = sf.openSession();
+        List<User> result = List.of();
         try {
-            session = sf.openSession();
             session.beginTransaction();
-            List<User> result =
-                    session.createQuery("from user order by id", User.class).list();
+            result = session.createQuery("from user order by id", User.class).list();
             session.getTransaction().commit();
-            session.close();
-            return result;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
+        return result;
     }
 
     /**
@@ -99,17 +92,18 @@ public class UserRepository {
      * @return пользователь.
      */
     public Optional<User> findById(int userId) {
+        Session session = sf.openSession();
+        Optional<User> result = Optional.empty();
         try {
-            session = sf.openSession();
             session.beginTransaction();
-            User result = session.get(User.class, userId);
+            result = Optional.of(session.get(User.class, userId));
             session.getTransaction().commit();
-            session.close();
-            return Optional.ofNullable(result);
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return Optional.empty();
+        return result;
     }
 
     /**
@@ -118,19 +112,20 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        List<User> result = List.of();
         try {
-            session = sf.openSession();
             session.beginTransaction();
-            List<User> result =
-                    session.createQuery("from user where login like :login", User.class)
+            result = session.createQuery("from user where login like :login", User.class)
                             .setParameter("login", key).list();
             session.getTransaction().commit();
-            session.close();
             return result;
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return null;
+        return result;
     }
 
     /**
@@ -139,17 +134,19 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        Optional<User> result = Optional.empty();
         try {
-            session = sf.openSession();
             session.beginTransaction();
-            User result = session.createQuery("from user where login = :login", User.class)
-                    .setParameter("login", login).uniqueResult();
+            result = session.createQuery("from user where login = :login", User.class)
+                    .setParameter("login", login).uniqueResultOptional();
             session.getTransaction().commit();
             session.close();
-            return Optional.ofNullable(result);
         } catch (Exception e) {
             session.getTransaction().rollback();
+        } finally {
+            session.close();
         }
-        return Optional.empty();
+        return result;
     }
 }
