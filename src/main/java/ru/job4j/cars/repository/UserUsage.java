e34b99b526cbb9da4940e98c4f972184c1a12b14
Nik Startsev <nikstar1235@gmail.com>
2023-12-17 21:01:19 +0300

    6. Модели и связи. Машины и владельцы [#4744]

--- src/main/java/ru/job4j/cars/repository/UserUsage.java
+++ src/main/java/ru/job4j/cars/repository/UserUsage.java
@@ -12,8 +12,8 @@ public class UserUsage {
                 .configure().build();
         try (SessionFactory sf = new MetadataSources(registry)
                 .buildMetadata().buildSessionFactory()) {
-        /*    var userRepository = new UserRepositoryOld(sf);
             var user = new User();
+        /*    var userRepository = new UserRepositoryOld(sf);
             user.setLogin("admin");
             user.setPassword("admin");
             userRepository.create(user);
